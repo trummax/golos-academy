41043956ce7e78cc7ad1fe2353e6d9cd3e7fb8d8
ninjascant <cvlt.mao@gmail.com>
2017-11-02 19:23:02 +0500

    Test commit

---
+++ ws-test.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+/*
+
+getCommits(cfrepo)
+		.then(commits => {
+			
+		.then(commits => {
+			commits = commits.filter(commit => commit.commit.message.search(/Add/)!== -1)
+			if(commits[0].sha!==lastCommitSHA) {
+				lastCommitSHA = commits[0].sha
+				let projectName = commits[0].message.search(/Add/)
+				if(projectName!==-1) {
+					projectName = projectName.match(/Add: (.*)/)[1]
+				}
+				readFile(dataRepo)
+					.then(res=>)
+			}
+			else 
+		})*/
